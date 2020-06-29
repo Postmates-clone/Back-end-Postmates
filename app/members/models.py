cbd77e9dfb338f826ccd68a78203d2984c877c09
jam <nevvjann@gmail.com>
2020-06-29 22:08:44 +0900

    Edit: add Meta data to User model

--- app/members/models.py
+++ app/members/models.py
@@ -15,3 +15,7 @@ class User(AbstractUser):
         upload_to=profile_img_dir, blank=True, help_text='프로필 사진')
     phone = models.CharField(
         max_length=13, help_text='핸드폰 번호')
+
+    class Meta:
+        verbose_name = '유저'
+        verbose_name_plural = '유저'
